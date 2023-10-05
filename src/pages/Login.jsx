6b549d625b8df147e45e62dc567a2eafd7b39522
unknown <lgu_lgu@naver.com>
2023-10-05 21:25:50 +0900

    login design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,40 +3,44 @@ import React, { useState } from 'react';
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+/*
   const handleLogin = () => {
     // 로그인 로직을 여기에 추가
     console.log(`Username: ${username}, Password: ${password}`);
   };
-
+*/
   return (
-    <div className='login-container'>
-      <h2>Login</h2>
-      <form className='login-form'>
-        <div className='form-group'>
-          <label className='label' htmlFor="username">Username:</label>
-          <input
-            className='input'
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    <div className="container right-panel-active">
+      <div className="container__form container--signup">
+        <form action="#" className="form" id="form1">
+          <h2 className="form__title">Sign Up</h2>
+          <input type="text" placeholder="User" className="input" />
+          <input type="email" placeholder="Email" className="input" />
+          <input type="password" placeholder="Password" className="input" />
+          <button className="btn">Sign Up</button>
+        </form>
+      </div>
+
+      <div className="container__form container--signin">
+        <form action="#" className="form" id="form2">
+          <h2 className="form__title">Sign In</h2>
+          <input type="email" placeholder="Email" className="input" />
+          <input type="password" placeholder="Password" className="input" />
+          <a href="#" className="link">Forgot your password?</a>
+          <button className="btn">Sign In</button>
+        </form>
+      </div>
+
+      <div className="container__overlay">
+        <div className="overlay">
+          <div className="overlay__panel overlay--left">
+            <button className="btn" id="signIn">Sign In</button>
+          </div>
+          <div className="overlay__panel overlay--right">
+            <button className="btn" id="signUp">Sign Up</button>
+          </div>
         </div>
-        <div className='form-group'>
-          <label className='label' htmlFor="password">Password:</label>
-          <input
-            className='input'
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button className='button' type="button" onClick={handleLogin}>
-          Login
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
