762b28f3f8f714c7b9878819159798f2d3de7614
unknown <lgu_lgu@naver.com>
2023-10-09 18:29:48 +0900

    firebase

---
+++ src/fbase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBh2vZhDRqQxAkK0YwSR7W8Sb5roDciRg",
+  authDomain: "login-2559a.firebaseapp.com",
+  projectId: "login-2559a",
+  storageBucket: "login-2559a.appspot.com",
+  messagingSenderId: "1048703529117",
+  appId: "1:1048703529117:web:ee06875bb5111962c22474"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { app, auth };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
+import { auth } from '../fbase';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-/*
-  const handleLogin = () => {
-    // 로그인 로직을 여기에 추가
+  const [sign, setSign] = useState(false);
+
+  const signInBtn = document.getElementById("signIn");
+  const signUpBtn = document.getElementById("signUp");
+  const container = document.querySelector(".container");
+
+  const ClickLogin = () => {
     console.log(`Username: ${username}, Password: ${password}`);
   };
-*/
+
+  const Onchangeinput = (e) => {
+    setUsername(e.value);
+    console.log(username)
+  }
+
+  const handlesign = () => {
+    setSign(!sign);
+  }
+
   return (
-    <div className="container right-panel-active">
+    <div className={`container ${sign ? 'right-panel-active' : ''}`}>
       <div className="container__form container--signup">
         <form action="#" className="form" id="form1">
           <h2 className="form__title">Sign Up</h2>
           <input type="text" placeholder="User" className="input" />
-          <input type="email" placeholder="Email" className="input" />
-          <input type="password" placeholder="Password" className="input" />
+          <input type="email" placeholder="Email" className="input" value={username} onChange={Onchangeinput} />
+          <input type="password" placeholder="Password" className="input" value={password} />
           <button className="btn">Sign Up</button>
         </form>
       </div>
@@ -24,8 +38,8 @@ const Login = () => {
       <div className="container__form container--signin">
         <form action="#" className="form" id="form2">
           <h2 className="form__title">Sign In</h2>
-          <input type="email" placeholder="Email" className="input" />
-          <input type="password" placeholder="Password" className="input" />
+          <input type="email" placeholder="Email" className="input" value={username} onChange={Onchangeinput} />
+          <input type="password" placeholder="Password" className="input" value={password} />
           <a href="#" className="link">Forgot your password?</a>
           <button className="btn">Sign In</button>
         </form>
@@ -34,10 +48,10 @@ const Login = () => {
       <div className="container__overlay">
         <div className="overlay">
           <div className="overlay__panel overlay--left">
-            <button className="btn" id="signIn">Sign In</button>
+            <button className="btn" id="signIn" onClick={handlesign}>Sign In</button>
           </div>
           <div className="overlay__panel overlay--right">
-            <button className="btn" id="signUp">Sign Up</button>
+            <button className="btn" id="signUp" onClick={handlesign}>Sign Up</button>
           </div>
         </div>
       </div>
